f4d00e42f61156ba5b20474710e49b37f0e79e64
YashK907 <yashkarande907@gmail.com>
2025-01-21 10:26:21 +0530

    Initial commit

---
+++ ktor-ConnectApp/src/main/kotlin/Serialization.kt
@@ -0,0 +1,32 @@
+package com.example
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.serialization.gson.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.sessions.*
+import kotlinx.serialization.Serializable
+import org.koin.dsl.module
+import org.koin.ktor.plugin.Koin
+import org.koin.logger.slf4jLogger
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        gson {
+            }
+    }
+    routing {
+        get("/json/gson") {
+                call.respond(mapOf("hello" to "world"))
+            }
+        get("/json/kotlinx-serialization") {
+                call.respond(mapOf("hello" to "world"))
+            }
+    }
+}
